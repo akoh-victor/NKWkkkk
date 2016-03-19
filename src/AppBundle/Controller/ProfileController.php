93959ad84a7ee8b972379f79541ead6c09d37167
Akoh Ojochuma Victor <akoh.chuma@gmail.com>
2016-03-19 21:55:40 +0100

    profile account/ activities/default creation

--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -20,18 +20,65 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 class ProfileController extends baseProfiler
 {
     /**
-     * @Route("/profile/show", name="show profile")
+     * @Route("/profile", name="showProfile")
      */
-    public function showAction()
+    public function showProfileAction()
     {
         $user = $this->getUser();
         if (!is_object($user) || !$user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
 
+        /*get settings and check for the default profile
+         if default profile page is account
+           set url to account page
+         else if default profile page is activities
+           set url to activities
+          */
+        //default hardcoded url == account
+        $url="/profile/account.html.twig";
+
         return $this->render('default/profile.html.twig', array(
             'user' => $user,
+            'default'=>$url
 
+        ));
+    }
+
+
+
+
+    /**
+     * @Route("/profile/account", name="showProfileAccount")
+     */
+    public function showAccountAction()
+    {
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        $url="1";
+
+        return $this->render('default/profile.html.twig', array(
+            'user' => $user,
+            'account'=>$url
+
+        ));
+    }
+    /**
+     * @Route("/profile/activities", name="showProfileActivities")
+     */
+    public function showActivityAction()
+    {
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+        $url="1";
+        return $this->render('default/profile.html.twig', array(
+            'user' => $user,
+            'activities'=>$url
         ));
     }
 }
